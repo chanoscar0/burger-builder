e5bf0c8888a519ec575b05f8f5c282abdf1bf966
Oscar Chan <chanoscar0@gmail.com>
2018-03-17 21:37:50 -0700

    Feature: Check for empty burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,21 +3,25 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 import classes from './Burger.css'
 
 const Burger = (props) => {
-  console.log(Object.keys(props.ingredients));
-  const tIngredients = Object.keys(props.ingredients)
+  let tIngredients = Object.keys(props.ingredients)
                        .map(ingKey => {
                          return [...Array(props.ingredients[ingKey])].map((_, i) => {
                            return <BurgerIngredient key={ingKey + i} type={ingKey} />;
-                           console.log(<BurgerIngredient key = {ingKey + i} type = {ingKey} />); //result is an Array with [meat0, meat1,cheese0,cheese1,salad0]
                          }); //[,] We have an array with 2 elements
-                       });
+                       })
+                       .reduce((arr,el) => {
+                          return arr.concat(el)
+                        }, []);
+                        console.log(tIngredients);
+  if (tIngredients.length ===0){
+    tIngredients = <p>Please start adding ingredients!</p>;
+  }
   return (
     <div className = {classes.Burger}>
       <BurgerIngredient type = "bread-top"/>
       {tIngredients}
       <BurgerIngredient type = "bread-bottom"/>
     </div>
-
   );
 }
 export default Burger;
