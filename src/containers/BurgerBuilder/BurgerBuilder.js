da56b1ce12b4ea6cdcc5c856d2ad05662ab7d83e
Oscar Chan <chanoscar0@gmail.com>
2018-03-28 14:12:28 -0700

    Feature: Routing for Entering an order

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -39,35 +39,13 @@ class BurgerBuilder extends Component{
     this.setState({purchasing: false})
   }
   purchaseContinueHandler = () => {
-    /*this.setState({loading: true});
-    //alert ('You continue!');
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer:{
-        name: 'Oscar Chan',
-        address: {
-          street: '1998 Maple Avenue',
-          zipcode: '12345',
-          country: 'United States'
-        },
-        email: 'test@test.com',
-      },
-      deliveryMethod: 'fastest'
-    }
-    instance.post('/orders.json', order)
-            .then(response => {
-              this.setState({loading: false, purchasing: false});
-            })
-            .catch(error => {
-              this.setState({loading: false, purchasing: false});
-            });
-            */
+
     const queryParams = [];
+
     for (let i in this.state.ingredients){
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
-    console.log(queryParams);
+    queryParams.push('price=' + this.state.totalPrice);
     const queryString = queryParams.join('&');
     this.props.history.push({
       pathname: '/checkout',
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
+import instance from '../../../axios-orders';
+import Spinner from '../../../components/UI/Spinner/Spinner';
+
 
 class ContactData extends Component {
   state = {
@@ -9,19 +12,52 @@ class ContactData extends Component {
     address: {
       street: '',
       postalCode: ''
+    },
+    loading: false
+  }
+  orderHandler = (event) => {
+    event.preventDefault();
+    this.setState({loading: true});
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.price,
+      customer:{
+        name: 'Oscar Chan',
+        address: {
+          street: '1998 Maple Avenue',
+          zipcode: '12345',
+          country: 'United States'
+        },
+        email: 'test@test.com',
+      },
+      deliveryMethod: 'fastest'
     }
+    instance.post('/orders.json', order)
+            .then(response => {
+              this.setState({loading: false});
+              this.props.history.push('/');
+            })
+            .catch(error => {
+              this.setState({loading: false});
+            });
+
   }
   render(){
+    let form = (
+      <form>
+        <input className = {classes.Input} type = 'text' name = 'name' placeholder = 'Your Name' />
+        <input className = {classes.Input} type = 'email' name = 'email' placeholder = 'Your email' />
+        <input className = {classes.Input} type = 'text' name = 'street' placeholder = 'Your Street Address' />
+        <input className = {classes.Input} type = 'text' name = 'postal' placeholder = 'Your Postal Code' />
+        <Button btnType = 'Success' clicked = {this.orderHandler}> ORDER</Button>
+      </form>
+    );
+    if (this.state.loading){
+      form = <Spinner />;    }
     return(
       <div className = {classes.ContactData}>
           <h4>Enter your contact data: </h4>
-          <form>
-            <input className = {classes.Input} type = 'text' name = 'name' placeholder = 'Your Name' />
-            <input className = {classes.Input} type = 'email' name = 'email' placeholder = 'Your email' />
-            <input className = {classes.Input} type = 'text' name = 'street' placeholder = 'Your Street Address' />
-            <input className = {classes.Input} type = 'text' name = 'postal' placeholder = 'Your Postal Code' />
-            <Button btnType = 'Success'> ORDER</Button>
-          </form>
+          {form}
       </div>
     )
   }
