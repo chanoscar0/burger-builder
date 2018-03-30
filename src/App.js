8d60aa1e1ec3ed2231e06e8e3dfcdb91defcbd36
Oscar Chan <chanoscar0@gmail.com>
2018-03-29 22:02:22 -0700

    Feature: Links

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Switch} from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders.js';
 class App extends Component {
   render() {
     return (
@@ -11,6 +12,9 @@ class App extends Component {
           <Switch>
             <Route path = '/' exact component = {BurgerBuilder} />
             <Route path = '/checkout'  component = {Checkout} />
+            <Route path = '/orders'  component = {Orders} />
+
+
           </Switch>
 
         </Layout>
