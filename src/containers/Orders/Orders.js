f9be8c8daaeae0a55f08e340ea8b3fc8222a59d8
Oscar Chan <chanoscar0@gmail.com>
2018-03-29 22:12:17 -0700

    Feature: Fetched Data of Orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react';
+import instance from '../../axios-orders';
 import Order from '../../components/Order/Order';
+import withErrorHandler from '../../components/withErrorHandler/withErrorHandler';
 class Orders extends Component {
+  state = {
+    orders: [],
+    loading: true
+  }
+  componentDidMount(){
+    const fetchedOrders = [];
+
+    instance.get('/orders.json')
+        .then(result => {
+          for (let key in result.data){
+            fetchedOrders.push({...result.data[key],
+            id: key});
+          }
+          console.log(fetchedOrders);
+          this.setState({loading: false, orders: fetchedOrders})
+        })
+        .catch(err => {
+          this.setState({loading: false})
+        })
+  }
   render(){
     return (
       <div>
@@ -10,4 +32,4 @@ class Orders extends Component {
     );
   }
 }
-export default Orders;
+export default withErrorHandler(Orders, instance);
