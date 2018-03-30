d2502f83bc7b45ee307bbc0f94e9db4a4b15177f
Oscar Chan <chanoscar0@gmail.com>
2018-03-29 22:31:33 -0700

    Feature: Orders Page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import classes from './Order.css'
-const Order = (props) => (
-  <div className = {classes.Order}>
-    <p>Ingredients: Salad (1)</p>
-    <p>Price: <strong>$5.45</strong></p>
-  </div>
-)
+const Order = (props) => {
+
+  const ingredientArray = [];
+  for(let ingredient in props.ingredients){
+    ingredientArray.push({name: ingredient,
+                     amount: props.ingredients[ingredient]});
+  }
+  console.log(ingredientArray);
+  let ingredientOutput = ingredientArray.map(ingredient => {
+    return <span style = {{textTransform: 'capitalize',
+                           display: 'inline-block',
+                           margin: '0 8px',
+                           border: '1px solid #ccc',
+                           padding: '5px'}}key = {ingredient.name}> {ingredient.name} ({ingredient.amount})</span>;
+  });
+  return(
+    <div className = {classes.Order}>
+      <p>Ingredients: {ingredientOutput}</p>
+      <p>Price: <strong>${Number.parseFloat(props.price).toFixed(2)}</strong></p>
+    </div>
+  );
+}
+
 
 
 export default Order;
