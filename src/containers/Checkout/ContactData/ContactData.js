2c18c88326ca36aafbe18e42dddffcfc1c517525
Oscar Chan <chanoscar0@gmail.com>
2018-03-30 16:41:33 -0700

    Feature: Dynamic Form Input

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -8,11 +8,54 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: ''
+    orderForm: {
+        name: {
+        elementType: 'input',
+      elementConfig: {
+        type: 'text',
+        placeholder: 'Your Name'
+      },
+      value: ''
+  },
+        street: {
+        elementType: 'input',
+      elementConfig: {
+        type: 'text',
+        placeholder: 'Street'
+      },
+      value: ''
+  },
+        zipcode: {
+        elementType: 'input',
+      elementConfig: {
+        type: 'text',
+        placeholder: 'Zipcode'
+      },
+      value: ''
+  },
+        country: {
+        elementType: 'input',
+      elementConfig: {
+        type: 'text',
+        placeholder: 'Country'
+      },
+      value: ''
+  },
+        email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Email'
+        },
+        value: ''
+  },
+        deliveryMethod: {
+        elementType: 'select',
+      elementConfig: {
+        options: [{value: 'fastest', displayValue: 'Fastest'},{value: 'cheapest', displayValue: 'Cheapest'}]
+      },
+      value: ''
+  },
     },
     loading: false
   }
@@ -22,16 +65,6 @@ class ContactData extends Component {
     const order = {
       ingredients: this.props.ingredients,
       price: this.props.price,
-      customer:{
-        name: 'Oscar Chan',
-        address: {
-          street: '1998 Maple Avenue',
-          zipcode: '12345',
-          country: 'United States'
-        },
-        email: 'test@test.com',
-      },
-      deliveryMethod: 'fastest'
     }
     instance.post('/orders.json', order)
             .then(response => {
@@ -43,13 +76,34 @@ class ContactData extends Component {
             });
 
   }
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    }
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+  }
+    updatedFormElement.value = event.target.value;
+    updatedOrderForm[inputIdentifier] = updatedFormElement;
+    this.setState({orderForm: updatedOrderForm});
+  }
   render(){
+    const formElementsArray = [];
+    for(let key in this.state.orderForm){
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      });
+    }
     let form = (
       <form>
-        <Input inputtype = 'input' type = 'text' name = 'name' placeholder = 'Your Name' />
-        <Input inputtype = 'input' type = 'email' name = 'email' placeholder = 'Your email' />
-        <Input inputtype = 'input' type = 'text' name = 'street' placeholder = 'Your Street Address' />
-        <Input  inputtype = 'input' type = 'text' name = 'postal' placeholder = 'Your Postal Code' />
+        {formElementsArray.map(formElement => (
+          <Input key = {formElement.id}
+                 elementType = {formElement.config.elementType}
+                 elementConfig = {formElement.config.elementConfig}
+                 value = {formElement.config.value}
+                 changed = {(event) => this.inputChangedHandler(event, formElement.id)}/>
+        ))}
         <Button btnType = 'Success' clicked = {this.orderHandler}> ORDER</Button>
       </form>
     );
