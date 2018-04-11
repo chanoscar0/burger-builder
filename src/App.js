1ea5e10109de618d19a7a919ab622379a8f1c398
Oscar Chan <chanoscar0@gmail.com>
2018-04-11 14:23:50 -0700

    Feature: Logout functionality

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import {Route, Switch} from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders.js';
+import Orders from './containers/Orders/Orders';
 import Auth from './containers/Auth/Auth';
+import Logout from './containers/Auth/Logout/Logout'
 class App extends Component {
   render() {
     return (
@@ -15,6 +16,7 @@ class App extends Component {
             <Route path = '/checkout'  component = {Checkout} />
             <Route path = '/orders'  component = {Orders} />
             <Route path = '/auth' component = {Auth} />
+            <Route path = '/logout' component = {Logout} />
 
           </Switch>
 
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,3 +1,3 @@
 export {addIngredient, removeIngredient, initIngredients} from './burgerBuilder';
 export {purchase, purchaseInit, fetchOrders} from './order';
-export {authStart, authSuccess, authFail, auth} from './auth';
+export {authStart, authSuccess, authFail, auth, logout} from './auth';
